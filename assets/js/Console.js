f9761910f9cffc95c6bd58e5f97d89dfa85ba5f7
Tony Gaitatzis <tonyg@backupbrain.org>
2013-09-12 11:37:21 -0500

    firt commit

--- assets/js/Console.js
+++ assets/js/Console.js
@@ -7,9 +7,19 @@ function Console(consoleElement) {
 	this.con = consoleElement;
 }
 Console.prototype.append = function(data) {
+	// only scroll to bottom if we are currently scrolled down
+	// if the user is browsing earlier content, don't force them down
+	// the page when the console updates
+	scrollToBottom = false
+	if (this.isAtBottom()) {
+		scrollToBottom = true;
+	}
 	this.con.html(this.con.html()+data);
-	this.scrollToBottom();
+	if (scrollToBottom) this.scrollToBottom();
 };
 Console.prototype.scrollToBottom = function() {
 	this.con.scrollTop(this.con[0].scrollHeight);
 };
+Console.prototype.isAtBottom = function() {
+	return (this.con[0].scrollHeight - this.con[0].clientHeight <= this.con.scrollTop());
+}
